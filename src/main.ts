fad977fa6721b29aa083e9075760647776ef157f
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-02-14 17:03:48 -0400

    agrega la configuracion de swagger a main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Impromec Calculador API')
+    .setDescription('API para el backend de Impromec Calculador')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({}));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
