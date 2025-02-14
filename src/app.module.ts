a3140d9f540ad1420f4161450ce2a83a1c09ca0e
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-02-14 11:50:19 -0400

    agrega resource auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
