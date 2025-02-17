b89b87111d0a903dc552073d606466303b27c1b8
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-02-17 14:13:14 -0400

    agrega configuracion de CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('Impromec Calculador API')
     .setDescription('API para el backend de Impromec Calculador')
@@ -17,11 +18,22 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Validación global
   app.useGlobalPipes(new ValidationPipe({}));
 
+  // Configuración de CORS
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+  const frontendUrl =
+    configService.get<string>('FRONTEND_URL') || 'http://localhost:3001';
+  const corsOptions = {
+    origin: [frontendUrl, 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
 
+  // Configuración de puerto y entorno
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
   console.log(`Running in ${process.env.NODE_ENV} mode on port ${port}`);
 }
