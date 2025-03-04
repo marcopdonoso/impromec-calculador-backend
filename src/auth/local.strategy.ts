36617a688db110e0ae8d7835db8ede9c44b50c21
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-03-03 22:33:56 -0400

    modifica el mensaje de error en local.stratefy.ts para login incorrecto

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -14,7 +14,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException('Credenciales inválidas');
+      throw new UnauthorizedException(
+        'Correo o contraseña incorrectos. Inténtalo de nuevo.',
+      );
     }
     return user;
   }
